210275cae0a4e557adf3dab238dde46a286420e3
David Faulkenberry <david.faulkenberry@gmail.com>
2015-12-07 17:29:57 -0500

    Some requisite files
    Copied from screaming-dinosaur

--- config/application.rb
+++ config/application.rb
@@ -6,18 +6,17 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Detours
+module ScreamingDinosaur
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.assets          false
+      g.helpers         false
+      g.stylesheets     false
+      g.template_engine :haml
+      g.test_framework  :rspec
+    end
+    config.encoding = 'utf-8'
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.filter_parameters += [:password, :spire, :secret, :github]
   end
 end
