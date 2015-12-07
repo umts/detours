5fb1af873d33a7dc148846d632d6c712b82e77d4
David Faulkenberry <david.faulkenberry@gmail.com>
2015-12-07 17:58:44 -0500

    List of properties

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,5 +1,8 @@
 class Route < ActiveRecord::Base
   has_and_belongs_to_many :posts
 
+  PROPERTIES = %w(Transit VATCo SATCo)
+
   validates :name, :number, :property, presence: true
+  validates :property, inclusion: { in: PROPERTIES }
 end
