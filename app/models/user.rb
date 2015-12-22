61907828eb61e16ce04a3e91bee1b3d770d17fda
David Faulkenberry <david.faulkenberry@gmail.com>
2015-12-22 16:36:18 -0500

    Specify start and end of string in SPIRE validation regex
    Closes #11.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   validates :first_name, :last_name, :spire, :email, presence: true
   validates :spire,
-            format: { with: /\d{8}@umass.edu/,
+            format: { with: /\A\d{8}@umass.edu\z/,
                       message: 'must be 8 digits followed by @umass.edu' }
   validates :spire, :email, uniqueness: true
 
